e87cb058b6d8ca68461e804c89b372e6a584cd84
Ezequiel Scigolini <escigoli@globalsyde.com>
2022-10-10 23:32:46 +0200

    graphQL server setup using type-graphQL

--- backend/README.txt
+++ backend/README.txt
@@ -1,2 +1,2 @@
-// terminal 1: npm start; npm run watch
+// terminal 1: npm run watch; 
 // terminal 2: npm run dev
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,45 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
+  // connect DDBB
   const orm = await MikroORM.init(mikroOrmConfig);
-  const post = orm.em.create(Post, { createdAt: new Date(), updatedAt: new Date(), title: 'my first post' })
-  await orm.em.persistAndFlush(post);
+  // run migrations
+  await orm.getMigrator().up();
+  // run SQL
+  // await RequestContext.createAsync(orm.em, async () => {
+  //   // Post
+  //   // const post = orm.em.create(Post, { createdAt: new Date(), updatedAt: new Date(), title: 'my first post' })
+  //   // await orm.em.persistAndFlush(post);
+
+  //   // Get
+  //   // const posts = await orm.em.find(Post, {});
+  //   // console.log(posts)
+  // });
+
+  const app = express();
+  
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    })
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log('server started on localhost:4000')
+  })
 }
 
 main().catch((err) => {console.error(err)});
 
-console.log("hello asd")
+console.log("Loading server...")
